44c1eed4bcbaf1a936a213904016d366ed411de7
Eliott-Sarrey <eliott.sarrey@gmail.com>
2022-10-26 14:57:53 +0200

    AddTitleBOM

--- hardware/scripts/GenerateReadMe.py
+++ hardware/scripts/GenerateReadMe.py
@@ -4,8 +4,10 @@ from markdownTable import markdownTable
 
 dirname = os.path.dirname(__file__)
 df = pd.read_csv(os.path.join(dirname, "../BillOfMaterials.csv"))
-md = markdownTable(df.to_dict(orient='records')).setParams(row_sep = 'markdown', padding_weight = 'centerleft', quote=False).getMarkdown()
+bom = markdownTable(df.to_dict(orient='records')).setParams(row_sep = 'markdown', padding_weight = 'centerleft', quote=False).getMarkdown()
+
+preamble = "# Bill of Materials"
 
 f = open(os.path.join(dirname, "../README.md"), "w")
-f.write(md)
+f.write(preamble + bom)
 f.close()
